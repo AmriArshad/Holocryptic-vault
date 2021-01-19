58b13c54f9df53c730078499ae8a98e5590cb208
AmriArshad <amri.arshad16@yahoo.com>
2021-01-19 15:42:41 +1300

    SquaresOfInfinity

---
+++ src/SquaresOfInfinity/sketch.js
@@ -0,0 +1,28 @@
+// Drawing infinity with squares
+// 19/01/2021
+
+let t = 0;
+const lines = 20;
+let size;
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  stroke(255);
+  strokeWeight(1);
+  noFill();
+  rectMode(CENTER);
+  size = width / 3;
+}
+
+function draw() {
+  background(0, 50);
+  translate(width / 2, height / 2);
+
+  for (let i = 0; i < lines; i++) {
+    let x1 = sin(t + i) * size;
+    let y1 = cos(t + i) * size * sin(t + i);
+
+    rect(x1, y1, size / 25);
+  }
+  t += 0.01;
+}
