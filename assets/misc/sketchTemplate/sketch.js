a07019ea2b1263071632b2ef85c472de3e701334
AmriArshad <amri.arshad16@yahoo.com>
2021-02-06 01:20:16 +1300

    updated sketch template

--- assets/misc/sketchTemplate/sketch.js
+++ assets/misc/sketchTemplate/sketch.js
@@ -1,9 +1,7 @@
 function setup() {
   createCanvas(1080, 1080);
-  // createCanvas(window.innerWidth, window.innerHeight);
-  // createCanvas(windowWidth, windowHeight);
 }
 
 function draw() {
-	
+  background(0);
 }
