3814650691078b3052abda866b4e82c50c5c2ba4
AmriArshad <amri.arshad16@yahoo.com>
2021-06-12 17:01:13 +1200

    21-X59

---
+++ src/2021/21-X59/sketch.js
@@ -0,0 +1,30 @@
+// 12/6/2021
+// Simple parametric equations
+
+let t = 0;
+const lines = 50;
+
+function setup() {
+  createCanvas(1080, 1080);
+  stroke(255, 10);
+  noFill();
+}
+
+function draw() {
+  background(0, 75);
+  translate(width / 2, height / 2);
+
+  beginShape();
+  for (let i = 0; i < lines; i += 0.05) {
+    let x1 = sin(t + i * 20) * 350;
+    let y1 = cos(t + i / 5) * 350;
+    let x2 = sin(-t + i / 20) * 350;
+    let y2 = cos(-t + i / 10) * 350;
+
+    vertex(x1, y1);
+    vertex(x2, y2);
+  }
+  endShape();
+
+  t += 0.005;
+}
