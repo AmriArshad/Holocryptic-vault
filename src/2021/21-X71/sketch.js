020b30cc423fa80fcccfac45c6dc2d3cc4addd0e
AmriArshad <amri.arshad16@yahoo.com>
2021-08-28 12:46:43 +1200

    21-X71

---
+++ src/2021/21-X71/sketch.js
@@ -0,0 +1,33 @@
+// Cyclic Pillars
+// 28/8/21
+
+let n = 999;
+let a = [];
+let r = 0;
+
+function setup() {
+  createCanvas(1080, 1080);
+  noStroke();
+  fill(255);
+
+  for (let i = 0; i < n * 2; i++) {
+    a[i] = random(width) - width / 2;
+  }
+}
+
+function draw() {
+  background(0, 50);
+
+  translate(width / 2, height / 2);
+
+  for (let i = 0; i < n * 2 - 1; i++) {
+    push();
+    let x = 250 * sin(mag(a[i], a[i + 1]) / 100 - r);
+    let y = 250 * tan(mag(a[i], a[i + 1]) / 20 - r);
+    translate(x, y);
+    ellipse(0, 0, 3);
+    pop();
+  }
+
+  r += .01;
+}
