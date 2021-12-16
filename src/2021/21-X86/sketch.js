3866f69602e63484d6c7bb034dcc2daa8565964e
AmriArshad <amri.arshad16@yahoo.com>
2021-12-17 11:10:34 +1300

    21-X86

---
+++ src/2021/21-X86/sketch.js
@@ -0,0 +1,26 @@
+// inspiration: https://twitter.com/incre_ment/status/1466829069853491204
+// Koru
+
+let t = 0
+let p, x, y;
+function setup() {
+  createCanvas(1080, 1080);
+  noStroke();
+  fill(255);
+  w = width / 2;
+}
+function draw() {
+  background(0);
+  translate(width / 2, height / 2);
+
+  for (let i = 0; i < 5000; i++) {
+    push();
+    p = (i + t) / 2500;
+    x = -w + p * width/2;
+    y = 9 * tan(x) * tan(x);
+    rotate(4 * PI * pow(map(mag(x, y), 0, w, sin(t*10), 0), 2));
+    circle(x, y, 2 + sin(PI * (8 * p - t)));
+    pop();
+  }
+  t += .001;
+}
